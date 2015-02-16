6c579d98211810034f3430e75e76a0c1d020ffe1
obb12 <omar.bb@hotmail.fi>
2015-02-16 09:52:11 +0200

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,9 +1,34 @@
-<?php  
-header('Content-type: application/json');
-?>
 <?php
+
+if ($_GET["data"]==="json") {
+	header('Access-Control-Allow-Origin: *');  
+header('Content-type: application/json; charset=utf-8');
+
+
 $date = $_GET['date'];
 $month = $_GET['month'];
-
 $output = shell_exec('curl  http://www.sodexo.fi/ruokalistat/output/daily_json/124/' . date('Y')  . '/' .  $month . '/' . $date . '/fi');
 echo "$output";
+}
+if ($_GET["data"]==="jsonp") {
+	 
+header('Content-type: application/javascript; charset=utf-8');
+
+
+$date = $_GET['date'];
+$month = $_GET['month'];
+$output = shell_exec('curl  http://www.sodexo.fi/ruokalistat/output/daily_json/124/' . date('Y')  . '/' .  $month . '/' . $date . '/fi');
+echo $_GET["callback"]  . "(" . "$output)";
+}
+if ($_GET["data"]==="xml") {
+	header('Access-Control-Allow-Origin: *');  
+header('Content-type: application/xml; charset=utf-8');
+
+
+
+$output = shell_exec('curl  http://www.sodexo.fi/ruokalistat/rss/weekly_rss/124/fi');
+echo "$output";
+}
+
+
+?>
